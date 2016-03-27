9ba4a77a934ce3816fa8f5bf5baa3c589da2acc5
stefanabogdanovic95 <stefibog95@hotmail.com>
2016-03-27 00:04:28 -0700

    Revert "Kompletirane su metode klase Menjacnica."
    This reverts commit c56a3861413900463d35477b3464cb72298a29fd.

--- Menjacnica1/src/menjacnica/Menjacnica.java
+++ Menjacnica1/src/menjacnica/Menjacnica.java
@@ -1,62 +1,24 @@
 package menjacnica;
 
 import java.util.GregorianCalendar;
-import java.util.LinkedList;
 
 import menjacnica.interfejs.MenjacnicaInterfejs;
 
 public class Menjacnica implements MenjacnicaInterfejs {
-	LinkedList<Valuta> valute = new LinkedList<>();
 
 	@Override
 	public void dodajKursValute(Valuta valuta, double prodajniKurs, double kupovniKurs, double srednjiKurs,
 			GregorianCalendar datum) {
-		Kurs k = new Kurs();
-		k.setDatum(datum);
-		k.setKupovniKurs(kupovniKurs);
-		k.setProdajniKurs(prodajniKurs);
-		k.setSrednjiKurs(srednjiKurs);
-		
-		for (int i = 0; i < valute.size(); i++) {
-			if (valute.get(i).equals(valuta)){
-				valute.get(i).getKursevi().add(k);
-				break;
-			}
-		}
 
 	}
 
 	@Override
 	public void obrisiKursValute(Valuta valuta, GregorianCalendar datum) {
-		for (int i = 0; i < valute.size(); i++) {
-			
-			if (valute.get(i).equals(valuta)){
-				
-				for (int j = 0; j < valuta.getKursevi().size(); j++) {
-					if (valuta.getKursevi().get(i).getDatum().equals(datum)){
-						valuta.getKursevi().remove(j);
-					}
-				}
-				
-			}
-		}
 
 	}
 
 	@Override
 	public Kurs vratiKursValute(Valuta valuta, GregorianCalendar datum) {
-		for (int i = 0; i < valute.size(); i++) {
-			
-			if (valute.get(i).equals(valuta)){
-				
-				for (int j = 0; j < valute.get(i).getKursevi().size(); j++) {
-					
-					if (valute.get(i).getKursevi().get(j).getDatum().equals(datum)){
-						return valute.get(i).getKursevi().get(j);
-					}
-				}
-			}
-		}
 		return null;
 	}
 
