91ae8de28833fa121264e8f45e79afc1f4bbefd1
stefanabogdanovic95 <stefibog95@hotmail.com>
2016-03-26 20:20:37 -0700

    Napravljene su odgovarajuce equals, hashCode i toString metode u obe klase.

--- Menjacnica1/src/menjacnica/Kurs.java
+++ Menjacnica1/src/menjacnica/Kurs.java
@@ -31,5 +31,52 @@ import java.util.GregorianCalendar;
 		public void setSrednjiKurs(String srednjiKurs) {
 			this.srednjiKurs = srednjiKurs;
 		}
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((datum == null) ? 0 : datum.hashCode());
+			result = prime * result + ((kupovniKurs == null) ? 0 : kupovniKurs.hashCode());
+			result = prime * result + ((prodajniKurs == null) ? 0 : prodajniKurs.hashCode());
+			result = prime * result + ((srednjiKurs == null) ? 0 : srednjiKurs.hashCode());
+			return result;
+		}
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Kurs other = (Kurs) obj;
+			if (datum == null) {
+				if (other.datum != null)
+					return false;
+			} else if (!datum.equals(other.datum))
+				return false;
+			if (kupovniKurs == null) {
+				if (other.kupovniKurs != null)
+					return false;
+			} else if (!kupovniKurs.equals(other.kupovniKurs))
+				return false;
+			if (prodajniKurs == null) {
+				if (other.prodajniKurs != null)
+					return false;
+			} else if (!prodajniKurs.equals(other.prodajniKurs))
+				return false;
+			if (srednjiKurs == null) {
+				if (other.srednjiKurs != null)
+					return false;
+			} else if (!srednjiKurs.equals(other.srednjiKurs))
+				return false;
+			return true;
+		}
+		@Override
+		public String toString() {
+			return "Kurs [datum=" + datum + ", prodajniKurs=" + prodajniKurs + ", kupovniKurs=" + kupovniKurs
+					+ ", srednjiKurs=" + srednjiKurs + "]";
+		}
+		
 
 }
